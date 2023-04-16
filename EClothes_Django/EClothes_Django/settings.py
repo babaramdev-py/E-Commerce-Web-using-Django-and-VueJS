11763ed8db2ce91a8c25062d4a6d121ddb10bc51
Ved K <vedkorla300@gmail.com>
2023-04-16 18:56:56 +0530

    Added a comment

--- EClothes_Django/EClothes_Django/settings.py
+++ EClothes_Django/EClothes_Django/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_ROOT = BASE_DIR / 'media/' #the place where the media files are stored
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- eclothes_vue/src/views/HomeView.vue
+++ eclothes_vue/src/views/HomeView.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="home">
-    Welcome to EClothes
+    <section class = "hero is-medium is-dark mb-6">
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">
+          Welcome to EClothes
+        </p>
+        <p class="subtitle">
+          An EClother built using Vue and Django
+        </p>
+      </div>
+    </section>
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered">
+          Latest Products
+        </h2>
+      </div>
+
+    </div>
   </div>
 </template>
 
