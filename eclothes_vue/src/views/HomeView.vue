c50ae25561359c677566b5ffb574bcf90a96d3d0
Ved K <vedkorla300@gmail.com>
2023-04-16 20:05:29 +0530

    Added images to products

--- eclothes_vue/src/views/HomeView.vue
+++ eclothes_vue/src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <section class = "hero is-medium is-dark mb-6">
+    <section class="hero is-medium is-dark mb-6">
       <div class="hero-body has-text-centered">
         <p class="title mb-6">
           Welcome to EClothes
@@ -10,12 +10,28 @@
         </p>
       </div>
     </section>
-    <div class="columns is-multiline">
+    <div class="columns is-multiline ">
+
       <div class="column is-12">
         <h2 class="is-size-2 has-text-centered">
           Latest Products
         </h2>
       </div>
+
+      <div class="column is-2" v-for="product in latestProducts" v-bind:key="product.id">
+
+        <div class="box">
+          <figure class="mb-6">
+            <img :src="product.get_thumbnail">
+          </figure>
+          <h3 class="is-size-4">{{ product.name }}</h3>
+          <p class="is-size-6 has-text-grey">{{ product.price }} $</p>
+          View Details
+        </div>
+        
+      </div>
+
+
 
     </div>
   </div>
@@ -23,11 +39,47 @@
 
 <script>
 // @ is an alias to /src
+import { labeledStatement } from '@babel/types'
+import axios from 'axios'
 
 
 export default {
   name: 'HomeView',
+  data() {
+    return {
+      latestProducts: [],
+    }
+  },
   components: {
+  },
+  // lifecyclehooks
+  mounted() {
+    this.getLatestProducts()
+  },
+  methods: {
+    getLatestProducts() {
+      axios
+        .get('api/v1/latest-products')
+        .then(response => {
+          this.latestProducts = response.data
+          console.log(this.latestProducts);
+        })
+        .catch(error =>{
+          console.log("Could not make calls to backend")
+
+        }
+      )
+    }
   }
+
 }
 </script>
+
+<style scoped>
+.image {
+  margin-top: -1.25rem;
+  margin-left: -1.25rem;
+  margin-right: -1.25rem;
+}
+</style>
+
