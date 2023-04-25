ba831e18acc9a8d31263ef1d93ac6b1668fcee08
Ved K <vedkorla300@gmail.com>
2023-04-25 19:22:49 +0530

    Modified animations

--- eclothes_vue/src/views/Checkout.vue
+++ eclothes_vue/src/views/Checkout.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="page-checkout animate__animated animate__bounceIn">
+    <div class="page-checkout animate__animated animate__bounceInUp">
         <div class="columns is-multiline">
             <div class="column is-12">
                 <h1 class="title">Check Out</h1>
