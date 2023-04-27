4ed198583facf7004f5d82400a080bcf1dceff76
Ved K <vedkorla300@gmail.com>
2023-04-27 16:07:55 +0530

    Changed main.js configs

--- eclothes_vue/src/main.js
+++ eclothes_vue/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
-axios.defaults.baseURL = 'http://127.0.0.1:8000/'
+axios.defaults.baseURL = 'https://vedkorla300.pythonanywhere.com/'
 
 createApp(App).use(store).use(router,axios).mount('#app')
 // Creates the VueApp
